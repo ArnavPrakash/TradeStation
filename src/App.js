2b4307eb69cd77a165fe48d27a6315202fe61606
Aditya Singh <aditya.l.k.2001@gmail.com>
2023-01-14 18:44:22 +0530

    Navbar Added plus Neev rkh di hai

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
+import Graph from './componants/graph.jsx';
+import Routed from './Router';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [sessionState, setSessionState] = useState({
+		userID: null,
+		userEmail: null,
+		active: null,
+		userName: null,
+		userImage: null
+	})
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+		<>
+			<BrowserRouter>
+      <Routes>
+        <Route exact path="/" element={<Navigate exact to="/login" />} />
+				<Route exact path="/login" element={<Routed/>} >
+					<Route path="login/graph" element={<Graph/>} />{" "}
+			  </Route>
+        </Routes>
+			</BrowserRouter>
+		</>
+	);
 }
 
 export default App;
