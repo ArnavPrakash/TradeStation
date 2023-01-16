e490d9f462e44d2e11891f1c7416544b888a54eb
ArnavPrakash <arnavprakash@iitbhilai.ac.in>
2023-01-16 07:38:44 +0530

    backend APIs done

---
+++ backend/env.sh
@@ -0,0 +1,4 @@
+echo "db_url=\"mongodb+srv://Joe:Jd7cOVWMnk76gLsn@tradestation.mauh5w9.mongodb.net/?retryWrites=true&w=majority\"" >> .env
+echo "PORT=8000" >> .env
+echo "HOST=0.0.0.0" >> .env
+echo "ROOT_PATH=\"\"" >> .env
