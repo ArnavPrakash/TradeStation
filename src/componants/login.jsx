72efed07725c9311012214a72c176b5586148923
Aditya Singh <aditya.l.k.2001@gmail.com>
2023-01-16 17:51:26 +0530

    Login and signup

--- src/componants/login.jsx
+++ src/componants/login.jsx
@@ -5,20 +5,16 @@ import './css/login.css'
 
 function Login() {
     
-    return(
-        <div><div className="container">
-        <form className="signUp">
-          <h3>Create Your Account</h3>
-          <p>Just enter your email address
-      and your password for join.
-          </p>
-          <input className="w100" type="email" placeholder="Insert eMail" reqired autocomplete='off' />
-          <input type="password" placeholder="Insert Password" reqired />
-          <input type="password" placeholder="Verify Password" reqired />
-          <button className="form-btn sx log-in" type="button" >Log In</button>
-          <button className="form-btn dx" type="submit" >Sign Up</button>
-        </form>
-      </div>
+    return(<div className="container">
+        <form class="signIn">
+    <h3>Welcome Back !</h3>
+    <button class="fb" type="button">Log In With Facebook</button>
+    <p>- or -</p>
+    <input type="email" placeholder="Insert eMail" autocomplete='off' reqired />
+    <input type="password" placeholder="Insert Password" reqired />
+    <button class="form-btn sx back" type="button">Back</button>
+    <button class="form-btn dx" type="submit">Log In</button>
+  </form>
       </div>
     );
   }
